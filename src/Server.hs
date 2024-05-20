d02ececfd0e0c440d4ed6c569a1f592b6ee2f5c8
Ben Lieberman <bhlieberman93@gmail.com>
2024-05-20 08:40:58 -0600

    working on getting the rest of the data

--- src/Server.hs
+++ src/Server.hs
@@ -2,12 +2,18 @@
 
 module Server (runScotty) where
 
+import Control.Exception
+import Control.Monad
 import Control.Monad.IO.Class
+import Data.Either
+import qualified Data.Text.Lazy as TL
+import Network.URI
 import Network.Wai.Middleware.Static
+import System.IO
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
-import Web.Scotty (ActionM, get, html, middleware, pathParam, scotty)
+import Web.Scotty (ActionM, get, html, middleware, parseParamList, pathParam, scotty, text)
 
 nameTemplate :: H.Html -> H.Html
 nameTemplate name = H.h1 $ "Hello" <> name
@@ -21,8 +27,33 @@ htmlHeader =
       head_ = H.head $ mconcat [charSet, name_, docTitle, css]
    in H.html head_
 
-getStaticData :: IO String
-getStaticData = readFile "resources/public/canto_I/thesis.txt"
+getStaticData :: FilePath -> IO String
+getStaticData = readFile
+
+getPoemTheses :: IO [String]
+getPoemTheses = forM ["canto_I", "canto_II", "canto_IV"] $ do
+  ( \n -> do
+      let path_ = "resources/public/" ++ n ++ "/thesis.txt"
+      getStaticData path_
+    )
+
+decodeParams :: String -> Either TL.Text [Int]
+decodeParams s = parseParamList . TL.pack $ unEscapeString s
+
+getCantoFootnotes :: Int -> [Int] -> IO [String]
+getCantoFootnotes canto footnote =
+  forM ["footnote_" ++ show filename ++ ".txt" | filename <- footnote :: [Int]] $ do
+    ( \f -> do
+        let canto_idx = ["I", "II", "IV"] :: [String]
+            romanNumeral = canto_idx !! canto
+            path_ = "resources/public/canto_" ++ romanNumeral ++ "/footnotes/" ++ f
+         in getStaticData path_
+              `catch` ( \e -> do
+                          let err = show (e :: IOException)
+                          hPutStr stderr err
+                          return ""
+                      )
+      )
 
 sampleData :: H.Html -> H.Html
 sampleData = H.script H.! A.type_ "text/plain"
@@ -46,8 +77,15 @@ runScotty :: IO ()
 runScotty = scotty 3000 $ do
   middleware $ staticPolicy (noDots >-> addBase "resources/public")
   get "/" $ do
-    thesis <- liftIO getStaticData :: ActionM String
-    html $ renderHtml $ mconcat [homeTemplate, sampleData $ H.toHtml thesis]
+    thesis <- liftIO getPoemTheses :: ActionM [String]
+    let theses = map (sampleData . H.toHtml) thesis
+    html $ renderHtml $ mconcat [homeTemplate, mconcat theses]
+  get "/footnotes/:canto/:footnote" $ do
+    canto <- pathParam "canto" :: ActionM Int
+    footnote <- pathParam "footnote" :: ActionM String
+    let parsed = fromRight [] $ decodeParams footnote
+    footnotes <- liftIO $ getCantoFootnotes canto parsed :: ActionM [String]
+    text $ mconcat $ map TL.pack footnotes
   get "/:name" $ do
     name <- pathParam "name" :: ActionM String
     html $ renderHtml $ nameTemplate $ H.toHtml name
--- ui/src/main/com/bhlie/nia/v2/core.cljs
+++ ui/src/main/com/bhlie/nia/v2/core.cljs
@@ -1,45 +1,55 @@
 (ns com.bhlie.nia.v2.core
   (:require [clojure.string :as str]
-            ["react" :refer [useEffect]]
             [reagent.core :as r]
             [reagent.dom.client :as rdc]))
 
 (defonce root (rdc/create-root (js/document.getElementById "root")))
 
-(defn timer [reset]
-  (let [now (. (js/Date. (js/Date.now)) toLocaleTimeString)
-        timer (js/setInterval #(reset now) 1000)]
-    (fn [] (js/clearInterval timer))))
+(defn get-script-tag [idx]
+  (let [tag (.item (.-scripts js/document) idx)
+        text (.-innerText tag)
+        [canto title & lines] (str/split-lines text)]
+    (into [:div
+           [:h1 canto]
+           [:h2 title]]
+          (for [line lines]
+            [:p line]))))
 
-(defn get-script-tag []
-  (into [:div]
-        (for [tag (.-scripts js/document)
-              :let [text (.-innerText tag)]
-              :when (not (str/blank? text))
-              :let [[canto title & lines] (str/split-lines text)]]
-          (into [:div 
-                 [:h1 canto] 
-                 [:h2 title]
-                 [:button "Next ->"]]
-                (for [line lines]
-                  [:p line])))))
+(defonce app-state (r/atom {:footnotes nil}))
 
-(defn clock []
-  (r/with-let [current-time (r/atom nil)]
-    (useEffect
+(defn get-footnotes [canto]
+  (r/with-let [cursor (r/cursor app-state [:footnotes])]
+    (-> (js/fetch (str "http://localhost:3000/footnotes/" canto))
+        (.then (fn [resp] (.text resp)))
+        (.then (fn [data] 
+                 (js/console.log data)
+                 (reset! cursor (str/split #"\n\n" data)))))))
+
+(defn router [canto]
+  [:div
+   [:button
+    {:on-click
      (fn []
-       (let [now (-> (js/Date.now) js/Date. .toLocaleTimeString)
-             timer (js/setInterval #(reset! current-time now) 1000)]
-         (fn [] (js/clearInterval timer)))))
-    [:div
-     [:p "Current time: " @current-time]]))
+       (when (not= @canto 1)
+         (swap! canto dec)))} "Previous"]
+   [:button
+    {:on-click
+     (fn []
+       (when (not= @canto 3)
+         (swap! canto inc)))} "Next"]])
 
 (defn root-comp []
-  [:div
-   [get-script-tag]])
+  (r/with-let [canto (r/atom 1)]
+    [:div
+     [router canto]
+     [get-script-tag @canto]]))
 
 (defn ^:dev/after-load render! []
   (rdc/render root [root-comp]))
 
 (defn init []
   (render!))
+
+(comment
+  @app-state
+  (get-footnotes 1))
