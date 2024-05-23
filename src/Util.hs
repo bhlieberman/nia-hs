1d83b80d358d28352cde6d0353298c3d9ec31056
Ben Lieberman <bhlieberman93@gmail.com>
2024-05-23 17:38:18 -0600

    starting on SVGs

--- src/Util.hs
+++ src/Util.hs
@@ -1,8 +1,20 @@
-module Util (mkCantoNumeral) where
+{-# LANGUAGE OverloadedStrings #-}
+
+
+module Util (mkCantoNumeral, renderSVG) where
 
 import qualified Data.Map as Map
+import Data.Text
+import System.IO
+import Text.Blaze ()
+import qualified Text.Blaze.Html5 as H
 
 mkCantoNumeral :: Int -> String
 mkCantoNumeral num =
   let cantoMap = Map.fromList [(1, "I"), (2, "II"), (4, "IV")] :: Map.Map Int String
    in cantoMap Map.! num
+
+renderSVG :: FilePath -> IO H.Html
+renderSVG fp =
+  let raw = readFile' fp
+   in H.toHtml . pack <$> raw
