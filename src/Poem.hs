7b6f8b1ffe275077c32c1fa4d8be2d57e2d84038
Ben Lieberman <bhlieberman93@gmail.com>
2024-05-23 06:44:14 -0600

    indexed lines for later image loading

--- src/Poem.hs
+++ src/Poem.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Poem (renderWholePoem) where
 
 import Control.Monad
 import Data (sortCantoDir, wholePoem)
 import Data.Maybe
+import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import System.Directory.Tree
+import Text.Blaze (textValue)
 import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 
 showWholePoem' :: IO [String]
 showWholePoem' = do
@@ -15,7 +20,7 @@ showWholePoem' = do
         fromJust $
           forM
             cnt
-            ( \c -> do
+            ( \c ->
                 let sorted_ = sortCantoDir c
                  in sorted_
             )
@@ -24,12 +29,14 @@ showWholePoem' = do
 renderWholePoem :: IO H.Html
 renderWholePoem = do
   whole <- showWholePoem'
+  let indexed = zip [1 ..] (concatMap lines whole) :: [(Int, String)]
   html <-
     forM
-      (concatMap lines whole)
-      ( \l ->
-          let txt = H.toHtml $ TL.pack l
-              elem_ = H.p txt
+      indexed
+      ( \(i, l) ->
+          let txt = H.toHtml $ TL.pack l :: H.Html
+              idVal = textValue $ T.pack $ "line-" ++ show i
+              elem_ = H.p H.! A.id idVal $ txt
            in pure elem_
       )
   return $ mconcat html
